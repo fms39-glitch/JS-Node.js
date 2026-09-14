a18ce88502420bb72b83ac4fd3a25ec4df777079
fms39-glitch <fms39@njit.edu>
2026-09-14 14:57:25 -0400

    Lec 2

---
+++ script.js
@@ -0,0 +1,36 @@
+//Data Types
+// Numbers
+
+
+
+//<<Data types:>>
+
+// 1. Numbers
+let num1 = 3; //The three will auto assign to a number.
+let num2 = 3.2
+console.log(num1 + num2);
+
+// 2. String   
+const firstName = "Faizan"
+let lastName = "Shaikh"
+console.log(lastName + ' ' + firstName)
+
+// 3. Booleans (true/false value)
+
+let isLoggedIn = true
+console.log(isLoggedIn);
+
+// 4. Null 
+let lastLoginDate = null; //The variable lastLoginDate don't know what it's type is could be anything in the future
+console.log(lastLoginDate)
+
+// 5. Undefined
+let a = undefined // Diff null is something that exist but it's not set to anything yet.
+console.log(a)
+
+// 6. Objects
+const person = {
+    firstName: 'Faizan',
+    lastName: 'Shaikh'
+}
+
