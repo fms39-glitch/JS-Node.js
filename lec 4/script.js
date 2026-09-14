69e1b060a01010f38fcc038bf647b644ecf4f774
fms39-glitch <fms39@njit.edu>
2026-09-14 17:33:12 -0400

    Logical Operators

---
+++ lec 4/script.js
@@ -0,0 +1,12 @@
+//Logical operator
+//AND --> &&
+// OR --> ||
+//NOT --> !
+
+const age = 20
+const gender = 'male'
+
+if (age >= 18 && gender == 'male') {
+    console.log("You are an Adult Male")
+}
+
